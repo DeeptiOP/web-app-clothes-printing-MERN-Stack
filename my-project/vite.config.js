24972b44933da5571f7480883e5cce56be64412a
Biswapriti Priyadarsini Jena <biswapritij15@gmail.com>
2025-08-23 02:13:59 +0530

    Fix asset loading with correct base path for GitHub Pages

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web-app-clothes-printing-MERN-Stack/',
   plugins: [react(), tailwindcss()],
   publicDir: 'public',
   build: {
